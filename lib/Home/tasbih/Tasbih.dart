66460d11d1d328841fa677d53d371d8f355c4e35
project calculator <ritamagdy99@gmail.com>
2023-05-09 19:54:38 +0300

    tasbih tab is finished

--- lib/Home/tasbih/Tasbih.dart
+++ lib/Home/tasbih/Tasbih.dart
@@ -1,38 +1,103 @@
+import 'package:eslami/My_Theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class TasbihTab extends StatelessWidget{
+class TasbihTab extends StatefulWidget{
+
+  @override
+  State<TasbihTab> createState() => _TasbihTabState();
+}
+
+class _TasbihTabState extends State<TasbihTab> {
+  int tasbehcounter =0;
+
+  List<String>TasbihList=[
+    "سبحان الله",
+    "لا اللة الا الله",
+    "الحمد لله",
+    "الله اكبر",
+    "لا حول ولا قوة الا بالله"];
+  int index=0;
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(
+      padding: EdgeInsets.all(100),
+      child: SizedBox(
+        width: double.infinity,
+
+        child:GestureDetector(
+          onTap: (){
+            onTasbihTab();
+          },
+          child: Column(
+            children: [
+              Image.asset('assets/images/seb7akbira.png'),
+              Padding(padding: EdgeInsets.all(16),),
+              GestureDetector(
+                onTap: (){
+                  onTasbihTab();
+                },
+                child:
+                Text("عدد التسابيح",
+                 style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold
+                ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.all(10),
+                  decoration: BoxDecoration(color: MythemeData.LightPrimary,
+                  borderRadius: BorderRadius.circular(26),
+                  ),
+                    child: Text("$tasbehcounter",
+                   style:
+                  TextStyle(fontSize: 30,
+                  fontWeight: FontWeight.bold
+                  ),),
+
+                ),
+              ),
+
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.all(8),
+                  decoration: BoxDecoration(color: MythemeData.LightPrimary,
+                    borderRadius: BorderRadius.circular(500),
+                  ),
+                  child: Text(TasbihList[index],
+                    style: TextStyle(fontSize: 30,
+                        fontWeight: FontWeight.bold
+                    ),),
+
+                ),
+              ),
 
 
-
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-
-        children: [
-
-
-
-          Image.asset('assets/images/seb7akbira.png'),
-
-          Padding(padding: EdgeInsets.all(10),),
-
-          Text('عدد النسابيح',textAlign: TextAlign.center, style: TextStyle(
-            fontSize: 25,
-
+              Padding(padding: EdgeInsets.all(35),),
+            ],
           ),
-          ),
-
-
-          Padding(padding: EdgeInsets.all(35),),
-
-
-
-
-        ],
+        ),
       ),
     );
   }
+
+
+ void onTasbihTab(){
+    tasbehcounter++;
+    if(tasbehcounter %33==0 ){
+      index++;
+    }
+    if(index == TasbihList.length)
+    {
+    index =0;
+    }
+    setState(() {
+    });
+
+ }
+
+
 }
---
+++ lib/provider/app_config_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class AppConfigProvider extends ChangeNotifier{
+
+
+  String appLangguage ='en';
+
+}
